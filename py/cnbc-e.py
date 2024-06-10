8bfad13f9c2045ead1fe7a53e87665a1465d717e
HyroxTV <121959126+hyroxtvvv@users.noreply.github.com>
2024-06-10 17:29:25 +0300

    Update cnbc-e.py

--- py/cnbc-e.py
+++ py/cnbc-e.py
@@ -1,6 +1,5 @@
 import requests
 import re
-import json
 
 base_url = "https://cnbc-e-live.ercdn.net/cnbce/"
 url = "https://www.cnbce.com/canli-yayin"
@@ -8,40 +7,29 @@ response = requests.get(url)
 
 if response.status_code == 200:
     site_content = response.text
-    match = re.search(r'ht_stream_m3u8":"(.*?)"', site_content)
+    match = re.search(r'liveUrl = \'(.*?)\'', site_content)
     
     if match:
-        json_data = match.group(1)
-        json_data_valid = json_data.replace("\\/", "/")  # Replace escaped slashes
+        baglanti = match.group(1)
+        #print(f"Location: {baglanti}")
+        content_response = requests.get(baglanti)
         
-        try:
-            ht_data = json.loads('{"ht_stream_m3u8":"' + json_data_valid + '"}')
-            ht_stream_m3u8 = ht_data.get('ht_stream_m3u8')
+        if content_response.status_code == 200:
+            content = content_response.text
+            lines = content.split("\n")
+            modified_content = ""
             
-            if ht_stream_m3u8:
-                #print(f"Found Live URL: {ht_stream_m3u8}")
-                content_response = requests.get(ht_stream_m3u8)
-                
-                if content_response.status_code == 200:
-                    content = content_response.text
-                    lines = content.split("\n")
-                    modified_content = ""
-                    
-                    for line in lines:
-                        if line.startswith("st"):
-                            full_url = base_url + line
-                            modified_content += full_url + "\n"
-                        else:
-                            modified_content += line + "\n"
-                    
-                    print(modified_content)
+            for line in lines:
+                if line.startswith("st"):
+                    full_url = base_url + line
+                    modified_content += full_url + "\n"
                 else:
-                    print("Error fetching content from the Live URL.")
-            else:
-                print("Live URL not found in the content.")
-        except json.JSONDecodeError as e:
-            print(f"JSON decoding error: {e}")
+                    modified_content += line + "\n"
+            
+            print(modified_content)
+        else:
+            print("Failed to fetch content.")
     else:
-        print("Live URL pattern not found in the content.")
+        print("Live URL not found in the content.")
 else:
-    print("Error: Status code is not 200.")
+    print("Failed to fetch the website content.")
